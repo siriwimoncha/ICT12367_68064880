89e4516025a688ae29a53a94355c0e5f067ea828
siriwimoncha <siriwimon.cha@spumail.net>
2026-03-23 14:46:21 +0700

    Add files via upload

---
+++ lab12/myproject/myapp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Person
+
+def about(request): return render(request, 'about.html')
+
+# แสดงข้อมูล
+def index(request):
+    persons = Person.objects.all()
+    return render(request, 'index.html', {'persons': persons})
+
+
+# เพิ่มข้อมูล
+def form_view(request):
+    if request.method == 'POST':
+        Person.objects.create(
+            firstname=request.POST.get('firstname'),
+            lastname=request.POST.get('lastname'),
+            nickname=request.POST.get('nickname'),
+            age=request.POST.get('age')  # 👈 เพิ่ม
+        )
+        return redirect('/')
+
+    return render(request, 'form.html')
+
+
+# แก้ไข
+def edit(request, id):
+    person = get_object_or_404(Person, id=id)
+
+    if request.method == 'POST':
+        person.firstname = request.POST.get('firstname')
+        person.lastname = request.POST.get('lastname')
+        person.nickname = request.POST.get('nickname')
+        person.age = request.POST.get('age')  # 👈 เพิ่ม
+        person.save()
+        return redirect('/')
+
+    return render(request, 'form.html', {'person': person})
+
+
+# ลบ
+def delete(request, id):
+    person = get_object_or_404(Person, id=id)
+    person.delete()
+    return redirect('/')
